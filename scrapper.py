17ccd78201c7933ca695a5ab6737f73762ade6c4
waqaarahmed <ahmedwaqar309@gmail.com>
2023-12-15 04:10:07 +0500

    new changes

--- scrapper.py
+++ scrapper.py
@@ -8,10 +8,15 @@ soup = BeautifulSoup(page.text, 'lxml')
 table = soup.find_all('table')[0]
 titles = table.find_all('th')
 table_title = [title.text.strip() for title in titles ]
+df = pd.DataFrame(columns = table_title)
 column = table.find_all('tr')
-for r in column:
+for r in column[1:]:
 	row = r.find_all('td')
 	row_data = [data.text.strip() for data in row]
-	print(row_data)
+	length = len(df)
+	df.loc[length] = row_data
+	print(df)
 
-#df = pd.DataFrame(columns = table_title)
+df.to_csv(r'file_location\companies.csv', index = False)
+
+
