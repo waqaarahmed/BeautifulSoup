ba4f7cc53b844bc42ffa7400448e8bfcd91c34a6
waqaarahmed <ahmedwaqar309@gmail.com>
2023-12-15 03:56:31 +0500

    new changes

--- scrapper.py
+++ scrapper.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup 
 import requests
+import pandas as pd
 
-url = 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_Pakistan'
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
-table = soup.find("table",{"class":"wikitable"})
-for title in table:
-	title = soup.find_all("th")
+table = soup.find_all('table')[0]
+titles = table.find_all('th')
+table_title = [title.text.strip() for title in titles ]
+column = table.find_all('tr')
+for r in column:
+	row = r.find_all('td')
+	row_data = [data.text.strip() for data in row]
+	print(row_data)
 
-table_title = [title.text.strip() for title in title]
-print(table_title)
+#df = pd.DataFrame(columns = table_title)
